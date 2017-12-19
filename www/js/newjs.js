c831920d6b5a7aad22db5735760ae6341202827c
Idowu Wasiu <adenwas@gmail.com>
2017-12-20 00:16:10 +0100

    Pushing returned values to appropriate container

--- www/js/newjs.js
+++ www/js/newjs.js
@@ -11,6 +11,14 @@ window.onload = function ()      {
                     if (request.responseText) {
                         var stringified = JSON.parse(request.responseText);
                         console.log(stringified);
+                        var location = document.getElementById("location");
+                        location.innerHTML = stringified.name + ", " +  stringified.sys.country;
+                        var temp = document.getElementById("temperature");
+                        temp.innerHTML = stringified.main.temp;
+                        var weatherCondition = document.getElementById("weather-condition");
+                        weatherCondition.innerHTML = stringified.weather[0].description;
+                        // var image = document.getElementById("image");
+                        // image.innerHTML = stringified.weather[0].icon;
                     } else {
                         console.log("Error: Can't get any data from that source");
                     }
@@ -20,3 +28,15 @@ window.onload = function ()      {
         });
     }
 };
+
+/* 
+Make the three circles align together on the same line.
+Add a span tag to the temperature div, make it clickable so it can be used for temperature conversion.
+Remove the date and time part, do enough to complete the challenge then get back to it to complete whatever accesories
+I want to add.
+Let the Weather update circle be at the top
+Find out how to make the returned icon the source of the image.
+Add background image
+Find a suitable color to give the circles so they match with the background image
+
+*/
