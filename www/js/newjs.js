40e961190853ccae9cf4d4849151a5f2a19304ce
Idowu Wasiu <adenwas@gmail.com>
2017-12-21 00:00:02 +0100

    Modifying color, background image, Local time and fonts

--- www/js/newjs.js
+++ www/js/newjs.js
@@ -1,42 +1,66 @@
-window.onload = function ()      {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function (position)        {
-            var longt = position.coords.longitude;
-            var latt = position.coords.latitude;
+window.onload = function () {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      var longt = position.coords.longitude
+      var latt = position.coords.latitude
 
-            var request = new XMLHttpRequest();
-            request.open('GET', 'https://fcc-weather-api.glitch.me/api/current?lat=' + latt + '&lon=' + longt, true);
-            request.onreadystatechange = function () {
-                if (request.readyState === request.DONE && request.status === 200) {
-                    if (request.responseText) {
-                        var stringified = JSON.parse(request.responseText);
-                        console.log(stringified);
-                        var location = document.getElementById("location");
-                        location.innerHTML = stringified.name + ", " +  stringified.sys.country;
-                        var temp = document.getElementById("temperature");
-                        temp.innerHTML = stringified.main.temp;
-                        var weatherCondition = document.getElementById("weather-condition");
-                        weatherCondition.innerHTML = stringified.weather[0].description;
-                        // var image = document.getElementById("image");
-                        // image.innerHTML = stringified.weather[0].icon;
-                    } else {
-                        console.log("Error: Can't get any data from that source");
-                    }
-                }
-            };
-            request.send();
-        });
-    }
-};
+      var request = new XMLHttpRequest()
+      request.open('GET', 'https://fcc-weather-api.glitch.me/api/current?lat=' + latt + '&lon=' + longt, true)
+      request.onreadystatechange = function () {
+        if (request.readyState === request.DONE && request.status === 200) {
+          if (request.responseText) {
+            var stringified = JSON.parse(request.responseText)
+            console.log(stringified)
+            var location = document.getElementById('location')
+            location.innerHTML = stringified.name + ', ' + stringified.sys.country
+            var temp = document.getElementById('temperature')
+            temp.innerHTML = stringified.main.temp + '&#176;'
+            var weatherCondition = document.getElementById('weather-condition')
+            weatherCondition.innerHTML = stringified.weather[0].description
+          // var image = document.getElementById("image")
+          // image.innerHTML = stringified.weather[0].icon
+          } else {
+            console.log("Error: Can't get any data from that source")
+          }
+        }
+      }
+      request.send()
+    })
+  }
+}
+
+function showTime () {
+  // body...
+  var date = new Date()
+  var hr = date.getHours()
+  var min = date.getMinutes()
+  var sec = date.getSeconds()
+  var session = 'AM';
+  if (hr === 0) {
+    hr = 12;
+  }
+
+  if (hr > 12) {
+    hr = hr - 12
+    session = 'PM'
+  }
+  hr = (hr < 10) ? '0' + hr : hr
+  min = (min < 10) ? '0' + min : min
+  sec = (sec < 10) ? '0' + sec : sec
+
+  var time = hr + ':' + min + ':' + sec + ' ' + session
+  document.getElementById('time').innerText = time
+  setTimeout(showTime, 1000)
+}
+
+showTime()
 
 /* 
-Make the three circles align together on the same line.
 Add a span tag to the temperature div, make it clickable so it can be used for temperature conversion.
-Remove the date and time part, do enough to complete the challenge then get back to it to complete whatever accesories
-I want to add.
 Let the Weather update circle be at the top
 Find out how to make the returned icon the source of the image.
-Add background image
 Find a suitable color to give the circles so they match with the background image
+Make a footer for the app.
+write a function for the conversion of celsius to fahrenheit
 
 */
