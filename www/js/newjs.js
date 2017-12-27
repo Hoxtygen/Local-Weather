c01086f57cd2a59aa8effdf3c0aaf21d6e738989
Idowu Wasiu <adenwas@gmail.com>
2017-12-28 00:33:38 +0100

    Made progress in converting celsius to fahrenheit

--- www/js/newjs.js
+++ www/js/newjs.js
@@ -12,50 +12,71 @@ window.onload = function () {
             var stringified = JSON.parse(request.responseText)
             console.log(stringified)
             var location = document.getElementById('location')
-            location.innerHTML = stringified.name + ', ' + stringified.sys.country;
+            location.innerHTML = stringified.name + ', ' + stringified.sys.country
             var temp = document.getElementById('temperature')
-            temp.innerHTML = stringified.main.temp + '&#176;';
-            var weatherCondition = document.getElementById('weather-condition');
-            weatherCondition.innerHTML = stringified.weather[0].description;
-          // var image = document.getElementById("image")
-          // image.innerHTML = stringified.weather[0].icon
+            temp.innerHTML = (stringified.main.temp).toFixed(0) + '&#176;'
+            var weatherCondition = document.getElementById('weather-condition')
+            weatherCondition.innerHTML = stringified.weather[0].description
+            var celsius = (stringified.main.temp).toFixed(0);
+            var btn = document.getElementById("btn");
+            var temp1 = convertToF(celsius);
+
+            function convertToF (celsius) {
+              var fahrenheit;
+              // Only change code below this line
+              fahrenheit = (celsius * 9 / 5) + 32;
+
+              // Only change code above this line
+              return fahrenheit + "&#176;";
+            }
+            convertToF(celsius);
+
+            btn.onclick = function () {
+              if (btn.innerText === "C") {
+                btn.innerText = "F";
+                temp.innerHTML = temp1;
+              }   else {
+                btn.innerText = "F";
+                btn.innerHTML = temp;
+              }
+            }
+
           } else {
-            console.log("Error: Can't get any data from that source");
+            console.log("Error: Can't get any data from that source")
           }
         }
       }
-      request.send();
+      request.send()
     })
   }
 }
 
-function showTime () {
-  // body...
-  var date = new Date();
-  var hr = date.getHours();
-  var min = date.getMinutes()
-  var sec = date.getSeconds()
-  var session = 'AM';
-  if (hr === 0) {
-    hr = 12;
-  }
+                            function showTime () {
+                                    // body...
+                                    var date = new Date()
+                                    var hr = date.getHours()
+                                    var min = date.getMinutes()
+                                    var sec = date.getSeconds()
+                                    var session = 'AM'
+                                    if (hr === 0) {
+                                      hr = 12
+                              }
 
-  if (hr > 12) {
-    hr = hr - 12
-    session = 'PM'
-  }
-  hr = (hr < 10) ? '0' + hr : hr;
-  min = (min < 10) ? '0' + min : min;
-  sec = (sec < 10) ? '0' + sec : sec;
+                              if (hr > 12) {
+                                hr = hr - 12
+                                session = 'PM'
+                              }
+                              hr = (hr < 10) ? '0' + hr : hr
+                              min = (min < 10) ? '0' + min : min
+                              sec = (sec < 10) ? '0' + sec : sec
 
-  var time = hr + ':' + min + ':' + sec + ' ' + session;
-  document.getElementById('time').innerText = time;
-  setTimeout(showTime, 1000);
-}
+                              var time = hr + ':' + min + ':' + sec + ' ' + session
+                              document.getElementById('time').innerText = time
+                              setTimeout(showTime, 1000)
+                            }
 
-showTime();
+                            showTime()
 
 /* 
-Add a span tag to the temperature div, make it clickable so it can be used for temperature conversion.
 Find out how to make the returned icon the source of the image.
 */
