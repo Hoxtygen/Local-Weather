6f62b56abb029ed9c25b416f4b542796d977472f
Idowu Wasiu <adenwas@gmail.com>
2017-12-29 02:45:05 +0100

    Refactored the design

--- www/js/newjs.js
+++ www/js/newjs.js
@@ -13,34 +13,36 @@ window.onload = function () {
             console.log(stringified)
             var location = document.getElementById('location')
             location.innerHTML = stringified.name + ', ' + stringified.sys.country
-            var temp = document.getElementById('temperature')
-            temp.innerHTML = (stringified.main.temp).toFixed(0) + '&#176;'
-            var weatherCondition = document.getElementById('weather-condition')
-            weatherCondition.innerHTML = stringified.weather[0].description
-            var celsius = (stringified.main.temp).toFixed(0);
-            var btn = document.getElementById("btn");
-            var temp1 = convertToF(celsius);
+            // var img = document.createElement('img');
+            // img.src = 'stringified.weather[0].icon';
+            document.getElementById('image-container').innerHTML = "<img src = 'stringified.weather[0].icon'>";
+            var temp = document.getElementById('temperature');
+            temp.innerHTML = (stringified.main.temp).toFixed(0) + '&#176;';
+            var weatherCondition = document.getElementById('weather-condition');
+            weatherCondition.innerHTML = stringified.weather[0].description;
+            var celsius = (stringified.main.temp).toFixed(0)
+            var btn = document.getElementById('btn')
+            var temp1 = convertToF(celsius)
 
             function convertToF (celsius) {
-              var fahrenheit;
+              var fahrenheit
               // Only change code below this line
-              fahrenheit = (celsius * 9 / 5) + 32;
+              fahrenheit = (celsius * 9 / 5) + 32
 
               // Only change code above this line
-              return fahrenheit + "&#176;";
+              return fahrenheit + '&#176;'
             }
-            convertToF(celsius);
+            convertToF(celsius)
 
             btn.onclick = function () {
-              if (btn.innerText === "C") {
-                btn.innerText = "F";
-                temp.innerHTML = temp1;
+              if (btn.innerText === 'C') {
+                btn.innerText = 'F'
+                temp.innerHTML = temp1
               }   else {
-                btn.innerText = "F";
-                btn.innerHTML = temp;
+                btn.innerText = 'F'
+                btn.innerHTML = temp
               }
             }
-
           } else {
             console.log("Error: Can't get any data from that source")
           }
@@ -51,32 +53,35 @@ window.onload = function () {
   }
 }
 
-                            function showTime () {
-                                    // body...
-                                    var date = new Date()
-                                    var hr = date.getHours()
-                                    var min = date.getMinutes()
-                                    var sec = date.getSeconds()
-                                    var session = 'AM'
-                                    if (hr === 0) {
-                                      hr = 12
-                              }
+function showTime () {
+  // body...
+  var date = new Date()
+  var hr = date.getHours()
+  var min = date.getMinutes()
+  var sec = date.getSeconds()
+  var session = 'AM'
+  if (hr === 0) {
+    hr = 12
+  }
 
-                              if (hr > 12) {
-                                hr = hr - 12
-                                session = 'PM'
-                              }
-                              hr = (hr < 10) ? '0' + hr : hr
-                              min = (min < 10) ? '0' + min : min
-                              sec = (sec < 10) ? '0' + sec : sec
+  if (hr > 12) {
+    hr = hr - 12
+    session = 'PM'
+  }
+  hr = (hr < 10) ? '0' + hr : hr
+  min = (min < 10) ? '0' + min : min
+  sec = (sec < 10) ? '0' + sec : sec
 
-                              var time = hr + ':' + min + ':' + sec + ' ' + session
-                              document.getElementById('time').innerText = time
-                              setTimeout(showTime, 1000)
-                            }
+  var time = hr + ':' + min + ':' + sec + ' ' + session
+  document.getElementById('time').innerText = time
+  setTimeout(showTime, 1000)
+}
 
-                            showTime()
+showTime()
 
 /* 
 Find out how to make the returned icon the source of the image.
 */
+/* var img = document.createElement("img")
+img.src = ""
+document.getElementById("image-container").appendChild(img); */
